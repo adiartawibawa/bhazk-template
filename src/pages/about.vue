5a55bf290b35fd1597becfd6d7f441ca1ace59b2
Adi Arta Wibawa <surat.buat.adi@gmail.com>
2022-11-16 06:13:44 +0800

    making auth page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
-        About Pages
+        <h1 class="text-6xl text-base-100">About Pages</h1>
+        <p class="pt-4 text-base-200">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae nostrum fugiat
+            maxime minus consequatur ipsa nisi dolorum enim. Mollitia ut molestias explicabo illo debitis ad, ab
+            asperiores, quam corrupti rerum ex architecto, alias recusandae adipisci odit nihil consequatur
+            voluptatibus? Fuga hic, laudantium nostrum facilis obcaecati, iure eum sequi minus est quibusdam voluptate
+            tenetur consequuntur dolorem molestias quam error aut, ipsa ad voluptatum. Tempora, sunt repudiandae?
+            Corporis ad in dolores eligendi quidem autem odio, a dicta sapiente error voluptates repellendus
+            reprehenderit corrupti dolore voluptatibus animi enim, inventore expedita accusantium! Sapiente quibusdam
+            aliquid assumenda quia? Animi vel quis soluta. Rerum, exercitationem? Perferendis?</p>
     </div>
 </template>
 
---
+++ src/pages/auth/register.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex min-h-full items-center justify-center py-24 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+            <div>
+                <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company">
+
+            </div>
+        </div>
+    </div>
+</template>
