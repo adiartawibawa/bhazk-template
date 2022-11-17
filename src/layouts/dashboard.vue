10dfaad083da17105dc58f6861f5920020b99f8e
Adi Arta Wibawa <surat.buat.adi@gmail.com>
2022-11-18 05:53:51 +0800

    add chart.js

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,11 +1,123 @@
 <script>
-
+import Chart from 'chart.js/auto';
 export default {
     data() {
         return {
             isOpen: false,
             dropdownOpen: false
         }
+    },
+    mounted() {
+        const charData = {
+            type: 'doughnut',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [20, 20, 20, 10, 10],
+                    backgroundColor: [
+                        'rgba(153, 102, 255)',
+                        'rgba(255, 206, 86)',
+                        'rgba(255, 99, 132)',
+                        'rgba(54, 162, 235)',
+                        'rgba(75, 192, 192)',
+                    ],
+                    borderColor: [
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(75, 192, 192, 1)',
+                    ],
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        position: 'bottom',
+                        fullSize: false,
+                        align: 'start',
+                        labels: {
+                            padding: 10,
+                            usePointStyle: true,
+                            pointSytle: 'circle'
+                        }
+                    }
+                },
+            }
+        };
+        const data = [
+            { x: 'Jan', applied: 19, left: 16 },
+            { x: 'Feb', applied: 15, left: 8 },
+            { x: 'Mar', applied: 24, left: 14 },
+            { x: 'Apr', applied: 8, left: 7 },
+            { x: 'May', applied: 11, left: 7 },
+            { x: 'Jun', applied: 10, left: 6 },
+            { x: 'Jul', applied: 17, left: 15 },
+            { x: 'Aug', applied: 19, left: 18 },
+            { x: 'Sep', applied: 18, left: 12 },
+            { x: 'Oct', applied: 19, left: 20 },
+            { x: 'Nov', applied: 18, left: 8 },
+            { x: 'Dec', applied: 17, left: 12 },
+        ];
+
+        const secondcharData = {
+            type: 'bar',
+            data: {
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                datasets: [{
+                    label: 'Applied students',
+                    borderRadius: 5,
+                    data: data,
+                    backgroundColor: '#FF7A00',
+                    maxBarThickness: 5,
+                    parsing: {
+                        yAxisKey: 'applied'
+                    },
+                },
+                {
+                    label: 'Left students',
+                    borderRadius: 5,
+                    maxBarThickness: 5,
+                    data: data,
+                    backgroundColor: '#6F52ED',
+                    parsing: {
+                        yAxisKey: 'left'
+                    },
+                },
+                ]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }],
+                    y: {
+                        beginAtZero: true,
+                        max: 25
+                    },
+                    x: {
+                        grid: {
+                            display: false
+                        }
+                    },
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                }
+            }
+        };
+        const ctx = document.getElementById('myChart');
+        const second = document.getElementById('secondChart');
+        const myChart = new Chart(ctx, charData);
+        const secondChart = new Chart(second, secondcharData);
+        myChart;
+        secondChart;
     }
 }
 </script>
